fe7a3c255c550295d93c0649c37eba68b5283b77
Alexander Erokhin <sascha_sobola@mail.ru>
2025-12-26 22:43:32 +0300

    add count hour parking +more thing

--- parking.py
+++ parking.py
@@ -1,4 +1,3 @@
-import random
 import numpy as np
 import sqlite3
 
@@ -10,14 +9,11 @@ CREATE TABLE IF NOT EXISTS Users (
 id INTEGER PRIMARY KEY,
 username TEXT NOT NULL,
 brand TEXT NOT NULL,
-time DATE,
-time_down DATE,
-flag INT
-)
+count_parking INT)
 ''')
 
-def insert(id, username, brand):
-    cursor.execute("INSERT INTO Users (id, username, brand) VALUES (?,?,?)", (id, username, brand))
+def insert(id, username, brand, count_parking):
+    cursor.execute("INSERT INTO Users (id, username, brand, count_parking) VALUES (?,?,?,?)", (id, username, brand, count_parking))
 
 def select():
     cursor.execute('SELECT * FROM Users')
@@ -26,11 +22,20 @@ def select():
     for i in users:
         print(i)
 
-def check(id_user):
+def check_user(id_user):
     cursor.execute('select 1 from Users where id = ? LIMIT 1', (id_user, ))
     return cursor.fetchone()
 
-np.random.seed(1)
+def brand_f(id_user):
+    cursor.execute('select brand from Users where id = ? limit 1', (id_user, ))
+    return cursor.fetchone()
+
+def count_parking_plus(id_user):
+    cursor.execute('Update Users set count_parking = count_parking + 1 where id = ?', (id_user, ))
+
+def delete_table():
+    cursor.execute('delete from Users')
+# np.random.seed(1)
 
 class Parking:
     def __init__(self, id, brand):
@@ -38,6 +43,8 @@ class Parking:
         self.brand = brand
         self.f = 1
         self.price = 100
+        self.price_end = 150
+        self._wastes = 0
         
     def id(self):
         return self._id
@@ -45,29 +52,27 @@ class Parking:
     def register():
         return np.random.randint(1000, 10000)
     
-    def enter(self, time_down, time):
-        pass
-    
-    def otdach(self):
-        pass
+    def parking_price(self, amount):
+        time_parking = np.random.randint(amount, amount+10)
+        count_parking_plus(self._id)
+        return (self.price * amount) + (self.price_end * (time_parking-amount))
 
-id_user = Parking.register()
-if check(id_user) is False:
-    name = input('Введите имя')  
+
+# id_user = Parking.register()
+id_user = int(input('Введите ID '))
+if check_user(id_user) is None:
+    name = input('Введите имя ')  
     brand_auto = input('Введите бренд вашего автомобиля ')
+    insert(id_user, name, brand_auto, 0)
 else:
     print('Вы зарегистрированы')
-# id_user = Parking.register()
 
-# insert(id_user, name, brand_auto)
+brand_auto = brand_f(id_user)
+car = Parking(id_user, brand_auto)
+amount = int(input('На сколько часов вы хотите арендовать парковочное место? '))
+print(f'Итого: {car.parking_price(amount)} рублей')
 
-
-
-# request = int(input('Выбере услугу: 1-въезд, 2-выезд'))
-
-# if 
-
-
-
+print(select())
+    
 connection.commit()
 connection.close()
