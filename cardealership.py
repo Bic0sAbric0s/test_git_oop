23ced8fdbd9c01c9958f066b9e94a61dd4c9f1b1
Alexander Erokhin <sascha_sobola@mail.ru>
2025-12-28 13:47:51 +0300

    +choice car

--- cardealership.py
+++ cardealership.py
@@ -14,32 +14,50 @@ auto_df = {}
 #     auto_df[np.random.randint(1000,9999)] = [np.random.choice(auto_m), np.random.randint(1_500_000, 7_000_000)]
 
 class CarDealerShip:
-    def __init__(self, mark, balance):
+    auto_df = {np.random.randint(1000,9999): [np.random.choice(auto_m), np.random.randint(1_500_000, 7_000_000), np.random.randint(2012, 2025), np.random.randint(75_000, 500_000)] for _ in range(30)}
+    id = list(auto_df.keys())
+    
+    def __init__(self, mark, balance, year, km):
         self.mark = mark
         self.balance = balance
-        self.auto_df = {np.random.randint(1000,9999): [np.random.choice(auto_m), np.random.randint(1_500_000, 7_000_000)] for _ in range(30)}
-        self.id = list(self.auto_df.keys())
+        self.year = year
+        self.km = km
         
-    def all_auto(self):
-        print('ID   brand  price')
-        for key, columns in self.auto_df.items():
-            print(f'{key}: {columns[0]}, {columns[1]}')
+    @staticmethod
+    def krit():
+        brand_user = input('Напишите марку автомобиля, который хотите арендовать ')
+        balance = int(input('Напишите сумму, в пределах которой вы хотите арендовать автомобиль '))
+        year = input('Введите год машины ').split('-')              
+        km = int(input('Какой пробег долен быть у машины '))
+        car = CarDealerShip(brand_user, balance, year, km) 
+        car.choice_brand()
+        
+    def all_auto():
+        print('ID   brand  price   year  distancekm')
+        for key, columns in CarDealerShip.auto_df.items():
+            print(f'{key}: {columns[0]}, {columns[1]}, {columns[2]}, {columns[3]}')
             
     def choice_brand(self):
-        for i in self.id:
-            if self.mark in self.auto_df[i][0]:
-                pass
+        for i in CarDealerShip.id:
+            if self.mark in CarDealerShip.auto_df[i][0] and \
+                CarDealerShip.auto_df[i][1] <= self.balance + 50_000 and\
+                (int(self.year[0]) <= CarDealerShip.auto_df[i][2] <= int(self.year[1])) and \
+                CarDealerShip.auto_df[i][3] <= self.km + 50_000:
+                    print(i, *CarDealerShip.auto_df[i])
+            else:
+                print('Ничего не найдено')
+                
+    def pay_car():
+        pass
             
+choice_user = int(input('Выберете: 1-показ всех авто, 2-выбор авто, 3-покупка авто'))
 
-                
-                
-# brand_user = input('Напишите марку автомобиля, который хотите арендовать ')
-# balance = input('Напишите сумму, в пределах который вы хотите арендовать автомобиль ')              
-    
-# car = CarDealerShip(brand_user, balance)
-car = CarDealerShip('2', 2)
-print(car.all_auto())
-# print(car.choice_brand())
+if choice_user == 1:
+    print(CarDealerShip.all_auto())               
+elif choice_user == 2:      
+    print(CarDealerShip.krit())
+elif choice_user == 3:
+    print(CarDealerShip.pay_car())
 
 
 
