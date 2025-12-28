fb48199a101b4c768eb87445069c1a8b07636f7a
Alexander Erokhin <sascha_sobola@mail.ru>
2025-12-28 16:42:06 +0300

    +dipsik

--- cardealership.py
+++ cardealership.py
@@ -7,38 +7,43 @@ import random
 np.random.seed(2)
 auto_m = ["Toyota", "Volkswagen", "Ford", "Honda", "Nissan", "Hyundai", "Kia", "Mercedes-Benz", "BMW", "Chevrolet"]
 
-auto_df = {}
-
 # Плохой вариант
 # for i in range(30):
 #     auto_df[np.random.randint(1000,9999)] = [np.random.choice(auto_m), np.random.randint(1_500_000, 7_000_000)]
 
 class CarDealerShip:
-    auto_df = {np.random.randint(1000,9999): [np.random.choice(auto_m), np.random.randint(1_500_000, 7_000_000), np.random.randint(2012, 2025), np.random.randint(75_000, 500_000)] for _ in range(30)}
+    auto_df = {
+        np.random.randint(1000,9999): 
+        [np.random.choice(auto_m),
+         np.random.randint(1_500_000, 7_000_000),
+         np.random.randint(2012, 2025), np.random.randint(75_000, 500_000)]
+        for _ in range(30)
+    }
     id = list(auto_df.keys())
     
     def __init__(self, mark, balance, year, km):
         self.mark = mark
         self.balance = balance
-        self.year = year
+        self.year = year  # должно быть: мин_год-макс_год
         self.km = km
    
         
-    @staticmethod
-    def krit():
+    @classmethod
+    def krit(cls):
         brand_user = input('Напишите марку автомобиля, который хотите арендовать ')
         balance = int(input('Напишите сумму, в пределах которой вы хотите арендовать автомобиль '))
         year = input('Введите год машины ').split('-')              
         km = int(input('Какой пробег долен быть у машины '))
-        car = CarDealerShip(brand_user, balance, year, km) 
-        car.choice_brand()
-    
         
-    def all_auto():
+        car = cls(brand_user, balance, year, km) 
+        return car.choice_brand()
+    
+    @classmethod  
+    def all_auto(cls):
         print('ID   brand  price   year  distancekm')
-        for key, columns in CarDealerShip.auto_df.items():
+        for key, columns in cls.auto_df.items():
             print(f'{key}: {columns[0]}, {columns[1]}, {columns[2]}, {columns[3]}')
- 
+        return len(cls.auto_df) 
             
     def choice_brand(self):
         auto_pay = {}
@@ -53,13 +58,13 @@ class CarDealerShip:
             print('Ничего не найдено')
             
         if auto_pay:
-            prom = int(input('Вы хотите рассмотреть данные варианты для покупки: 1-Да 2-Нет'))
+            prom = int(input('Вы хотите рассмотреть данные варианты для покупки: 1-Да 2-Нет '))
             if prom == 1:
                 CarDealerShip.pay_car(auto_pay)
             else:
                 print('Хорошего дня! Приходите к нам еще')
         
-                
+    @staticmethod            
     def pay_car(auto_pay):
         for key, col in auto_pay.items():
             print(f'ID: {key} Характеристики: {col[0]} {col[1]} {col[2]} {col[3]}')
@@ -67,18 +72,35 @@ class CarDealerShip:
         for i in list(auto_pay.keys()):
             if id_pay == int(i):
                 print(f'Поздравляю! Вы купили {auto_pay[i][0]} {auto_pay[i][2]} года с пробегом {auto_pay[i][3]}км')
-                break
+            
+            del CarDealerShip.auto_df[id_pay]
+            break
         else:
             print('Увы, автомобиля с таки ID нет')
             
-choice_user = int(input('Выберете: 1-показ всех авто, 2-выбор авто, 3-покупка авто '))
 
-if choice_user == 1:
-    print(CarDealerShip.all_auto())               
-elif choice_user == 2:      
-    print(CarDealerShip.krit())
-elif choice_user == 3:
-    print(CarDealerShip.pay_car(auto_df))
-
+if __name__ == "__main__":
+    while True:
+        print("\n" + "=" * 40)
+        print("АВТОСАЛОН")
+        print("=" * 40)
+        print("1. Показать все автомобили")
+        print("2. Подобрать автомобиль по критериям")
+        print("3. Выход")
+        
+        try:
+            choice_user = int(input('Введите число 1-3 '))
+        
+            if choice_user == 1:
+                CarDealerShip.all_auto()
+            elif choice_user == 2:
+                CarDealerShip.krit()
+            elif choice_user == 3:
+                print('До скорой встречи!')
+                break
+            else:
+                print('Пожалуйста, выберете 1,2 или 3 ')
+        except:
+            print('Вы ввели не число!')
 
 
